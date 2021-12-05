2ee0aa4f365789a19f40cf506f76ce1873dbbcde
MateoAcevedoGlowicom <mateoacevedo.glowicom@gmail.com>
2021-12-04 22:49:25 -0400

    fix method to active card in Card.tsx and img to add in the README.md

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,39 +10,40 @@ import {
 } from './CardStyles';
 
 interface CardProps {
+  id: number;
   title: string;
   description: string;
   from: string;
   to: string;
   selectCard: (e: MouseEvent<HTMLDivElement>, card: any) => void;
+  activeCard: undefined | number;
 }
 function Card(props: CardProps) {
-  const { title, description, from, to, selectCard } = props;
-  const [selected, setSelected] = useState(false);
+  const { id, title, description, from, to, selectCard, activeCard } = props;
   const handleClick = (e: MouseEvent<HTMLDivElement>) => {
-    setSelected(!selected);
-    selectCard(e, { title, description, from, to });
+    selectCard(e, { id, title, description, from, to });
   };
   return (
-    <CardContainer
-      isActive={selected}
-      onClick={(e) => handleClick(e)}
-      onMouseOut={(e) => setSelected(!selected)}
-    >
-      <div>
-        <CardTitle>{title}</CardTitle>
-        <CardText>{description}</CardText>
-        <CardTime>
-          {from}
-          <span> * </span>
-          {to}
-        </CardTime>
-      </div>
-      <CardStatus>
-        <CardStatusIconCheck isActive={selected} />
-        <CardStatusBadge>Open</CardStatusBadge>
-      </CardStatus>
-    </CardContainer>
+    <>
+      <CardContainer
+        isActive={id === activeCard ? true : false}
+        onClick={(e) => handleClick(e)}
+      >
+        <div>
+          <CardTitle>{title}</CardTitle>
+          <CardText>{description}</CardText>
+          <CardTime>
+            {from}
+            <span> * </span>
+            {to}
+          </CardTime>
+        </div>
+        <CardStatus>
+          <CardStatusIconCheck isActive={id === activeCard ? true : false} />
+          <CardStatusBadge>Open</CardStatusBadge>
+        </CardStatus>
+      </CardContainer>
+    </>
   );
 }
 
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,6 +3,7 @@ import { Container } from './ContainerStyles';
 import Card from '../Card/Card';
 
 interface infoCard {
+  id: number;
   title: string;
   description: string;
   from: string;
@@ -11,18 +12,21 @@ interface infoCard {
 
 const infoCards: Array<infoCard> = [
   {
+    id: 1,
     title: 'Main Office 1',
     description: '123 Main Street, Anytown, Puerto Rico',
     from: '3:00 AM',
     to: '7:00 PM',
   },
   {
+    id: 2,
     title: 'Main Office 2',
     description: '123 Main Street, Anytown, USA',
     from: '9:00 AM',
     to: '8:00 PM',
   },
   {
+    id: 3,
     title: 'Main Office 3',
     description: '123 Main Street, Anytown, Canada',
     from: '12:00 AM',
@@ -31,25 +35,31 @@ const infoCards: Array<infoCard> = [
 ];
 function ContainerComponent() {
   const [selectedCard, setSelectedCard] = useState<infoCard | null>(null);
-  const selectCardAndSendToState = (e: MouseEvent<HTMLDivElement>, card: infoCard) => {
+  const selectCardAndSendToState = (
+    e: MouseEvent<HTMLDivElement>,
+    card: infoCard
+  ) => {
     e.preventDefault();
     setSelectedCard(card);
   };
   return (
     <>
-   
-    <Container>
-
-    {selectedCard?.title}
-    {selectedCard?.description}
-    {selectedCard?.from}
-    
-    {selectedCard?.to}
-
-      {infoCards.map((card, index) => (
-          <Card key={index} {...card} selectCard={(e, card ) => selectCardAndSendToState(e, card)}  />
-      ))}
-    </Container>
+      <Container>
+        
+        <h1>{selectedCard ? 'Card Selected' : 'No Card Selected'}</h1>
+        <p>{selectedCard?.title}</p>
+        <p>{selectedCard?.description}</p>
+        <p>{selectedCard?.from}</p>
+        <p>{selectedCard?.to}</p>
+        {infoCards.map((card, index) => (
+          <Card
+            key={index}
+            {...card}
+            selectCard={(e, card) => selectCardAndSendToState(e, card)}
+            activeCard={selectedCard?.id}
+          />
+        ))}
+      </Container>
     </>
   );
 }
